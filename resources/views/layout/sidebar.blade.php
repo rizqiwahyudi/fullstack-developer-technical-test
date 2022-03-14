8faa832f79d373643277aa08f7eb2ce48d0cefda
ZD Aprillyo <zdaprillyo@gmail.com>
2022-03-14 09:18:44 +0700

    update-logo

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,9 +2,9 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('/') }}">
         <div class="sidebar-brand-icon">
-        <img src="img/logo/logo2.png">
+        <img src="img/logo/logo3.png">
         </div>
-        <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+        <div class="sidebar-brand-text mx-3"></div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
